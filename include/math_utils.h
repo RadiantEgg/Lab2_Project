44ce56e9464fd4e3bc52df7ff0d984075f1e935c
radiantegg <2173590735@qq.com>
2026-03-13 20:47:56 +0800

    完成阶乘代码

---
+++ include/math_utils.h
@@ -0,0 +1,6 @@
+#ifndef MATH_UTILS_H
+#DEFINE MATH_UTILS_H
+
+int factorial(int n);
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "math_utlis.h"
+
+int main()
+{
+    int n;
+    printf("请输入一个整数:");
+    scanf("%d", &n);
+
+    if (n < 0) {
+        printf("负数没有阶乘！\n");
+        return -1;
+    } else {
+        printf("%d的阶乘是:%d\n", n, factorial(n));
+    }
+    return 0;
+}
